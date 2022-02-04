a2be3660b4d0d3d703f914cd68ecd60bc25f9b52
Seraph <42664208+Seraph27@users.noreply.github.com>
2022-02-03 20:37:55 -0800

    fix test

--- skilldemoTester.java
+++ skilldemoTester.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class skilldemoTester {
     @Test
     public void multiply() {
-        assertEquals(2, skilldemo.multiply(2, 2));
+        assertEquals(4, skilldemo.multiply(2, 2));
     }
 }
