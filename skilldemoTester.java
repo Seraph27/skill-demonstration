1e06aa044aa4d025cc90c1c176022d7607590d1e
Seraph <42664208+Seraph27@users.noreply.github.com>
2022-02-03 20:35:21 -0800

    first test

---
+++ skilldemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemoTester {
+    @Test
+    public void multiply() {
+        assertEquals(2, skilldemo.multiply(2, 2));
+    }
+}
